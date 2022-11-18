6f4084ac8ce66ad120bd328c166a821bee6c08fe
Kyle Verhoog <kyle@verhoog.ca>
2022-11-18 14:18:49 -0500

    Only iterate on unique closed issues

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ if GH_TOKEN is None:
     logging.warning("not Github token set, likely to hit api limit")
 
 
-
-
 def repo_filenames(repo: git.Git) -> List[str]:
     return repo.ls_files().split("\n")
 
@@ -59,7 +57,7 @@ class FileIssue(NamedTuple):
         return repr(str(self))
 
 
-def find_issues_in_filenames(file_names: List[str]) -> List[FileIssue]:
+def find_issues_in_filenames(filenames: List[str]) -> List[FileIssue]:
     upstream_issues = []
     for filename in filenames:
         with open(filename) as file:
@@ -107,22 +105,22 @@ def get_closed_issues(
         try:
             gh_repo = gh.get_repo(repo)
         except UnknownObjectException:
-            logging.error("failed to look up issue %r", issue.ref)
+            logging.error("failed to look up repo %r", repo)
         else:
             for issue in issues:
                 try:
                     gh_issue = gh_repo.get_issue(number=issue.num)
-                except github.GithubException.UnknownObjectException:
+                except UnknownObjectException:
                     logging.error("failed to look up issue %r", issue.ref)
                 else:
-                    if gh_issue.state == "closed" and gh_issue not in [ghi[1] for ghi in closed_issues]:
+                    if gh_issue.state == "closed" and gh_issue not in [
+                        ghi[1] for ghi in closed_issues
+                    ]:
                         closed_issues.append((issue, gh_issue))
     return closed_issues
 
 
-def get_unique_issues(
-    issues: List[FileIssue]
-) -> Dict[str, List[FileIssue]]:
+def get_unique_issues(issues: List[FileIssue]) -> Dict[str, List[FileIssue]]:
     """Return issues without duplicates based on ref"""
     dedupe_issues = {}
     for issue in issues:
@@ -135,19 +133,7 @@ def get_unique_issues(
     return dedupe_issues
 
 
-def get_issue_locations(
-    issues: Dict[str, List[FileIssue]], closed_issue: FileIssue
-) -> List[Tuple[str, int]]:
-    """Return all the name and line number of all the files that contain the issue"""
-    locations = []
-    for ref, issues in unique_issues.items():
-        for issue in issues:
-            if issue.ref == closed_issue.ref:
-                locations.append((issue.filename, issue.lineno))
-    return locations
-
-
-if __name__ == "__main__":
+def main():
     g = git.Git()
     all_filenames = repo_filenames(g)
     filenames = filter_filenames(repo_filenames(g))
@@ -159,22 +145,22 @@ if __name__ == "__main__":
     closed_issues = get_closed_issues(issues, gh)
     logging.info("found %r closed issues", len(closed_issues))
 
+    unique_closed_issues = get_unique_issues([ci[0] for ci in closed_issues])
+    logging.debug("unique closed issues: %r", unique_closed_issues)
+
     GH_REPO = os.getenv("GITHUB_REPOSITORY")
     gh_repo = gh.get_repo(GH_REPO)
     repo_ref = os.getenv("GITHUB_REF_NAME")
     server_url = os.getenv("GITHUB_SERVER_URL")
 
     repo_issues = gh_repo.get_issues()
-    unique_issues = get_unique_issues(issues)
-    for (issue, gh_issue) in closed_issues:
-        locations = get_issue_locations(unique_issues, issue)
-
-        
+    for issue_ref, issues in unique_closed_issues.items():
+        locations = [(i.filename, i.lineno) for i in issues]
         files_str = "\n".join(
             f"  - [{fn}:{ln}]({server_url}/{GH_REPO}/blob/{repo_ref}/{fn}#L{ln})"
             for fn, ln in locations
         )
-        body = f"""Upstream issue {issue.ref} referenced in the file{'s' if len(locations) > 1 else ''}:
+        body = f"""Upstream issue {issue_ref} referenced in the file{'s' if len(locations) > 1 else ''}:
 
 {files_str}
 
@@ -184,7 +170,7 @@ The code referencing this issue could potentially be updated.
     """
         for repo_issue in repo_issues:
             if not DRY_RUN:
-                if issue.ref in repo_issue.title or issue.ref in repo_issue.body:
+                if issue_ref in repo_issue.title:
                     # repo issue already exists for the upstream issue, update it
                     # in case any references have been removed.
                     repo_issue.edit(
@@ -192,14 +178,27 @@ The code referencing this issue could potentially be updated.
                     )
                     break
             else:
-                logging.info(f"Would edit issue number {repo_issue.number} in repo `{GH_REPO}`:\nUpstream issue {issue.ref}\n\n{body}"),
+                logging.info(
+                    f"Would edit issue number {repo_issue.number} in repo `{GH_REPO}`:\nUpstream issue {issue_ref}\n{body}"
+                ),
 
         else:
             if not DRY_RUN:
                 gh_repo.create_issue(
-                    title=f"Upstream issue {issue.ref} closed",
+                    title=f"Upstream issue {issue_ref} closed",
                     body=body,
                     labels=LABELS,
                 )
             else:
-                logging.info(f"Would create issue in repo '{GH_REPO}':\nUpstream issue {issue.ref}\n\n{body}"),
+                logging.info(
+                    f"Would create issue in repo '{GH_REPO}':\nUpstream issue {issue_ref}\n{body}"
+                ),
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Used for testing
+# Blocked on https://github.com/Kyle-Verhoog/upstream-issue-notifier/issues/2
+# Blocked on https://github.com/Kyle-Verhoog/upstream-issue-notifier/issues/3
